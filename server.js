455e14367a2efd764bf58784de74bd17d155d5d3
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2020-09-17 19:20:15 -0300

    Organizing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import Scraper from './src/Utils/Scraper.js'
+
 const port = 3000
 const app = express()
 
@@ -7,14 +8,28 @@ app.get('/', async (req, res) => {
 
   if (!req.query.repository) return res.json({
     tutorial: 'use a get query named \'repository\' to pass the public github repository url(or folder)',
+    complete_example: 'http://url.domain/?repository=matheusdf32/spotify-finder',
     examples:[ 
       '?repository=matheusdf32/spotify-finder',
       '?repository=Rocketseat/react-native-template-rocketseat-basic/tree/master/src'
-    ]
+    ],
+    notes: {
+      code: "The code was made as simple as possible trying to follow the SOLID principles.",
+      parameters: "get query was chosen to be a parameter instead of post for simple use in any browser. Easy testing is happy testing.",
+      429: "Because of the nature behind web scraping, github might throw a 429 'too many requests' error. The repository 'microsoft/terminal' was fun to test with, sometimes it works and sometimes it doesn't. The best time scraping this repository was 66.717 seconds.",
+      recursive: "The simple approach also means i did not use any sort of pause-timers to avoid the 429 error and used a recursive approach to simplify the code but also making it work in any repository",
+      flaws: "There probably are some flaws in this code but there are none that I know that are my fault. Maybe the repository you use to benchmark it will show one of them. Trying to fix every error that you don't even know exists is a heavy task by itself, but that's part of the challenge!",
+      cheerio: "I Sent an email, asking about if CheerioJS(basically jQuery for backend nodejs) was considered a web scraping library because I had the intent of using it. I didn't consider it a web scraping library at first but since it's name was always mentioned in web scraping tutorials and documents, I chose to NOT use it.",
+      axios_and_nodejs: "I used axios and NodeJS just because I was familiar with them.",
+      ES6: "ECMAscript 2015 in NodeJS(v12 LTS) is still an Experimental module, that said, I enabled it.",
+      dotNAME: "Some files don't have extensions. They shouldn't show in the response. examples: '.gitignore' and '.editorconfig'",
+      thanks: "In or out, I did not had so much fun building an API in months! Thank you for reviewing my resume and giving me this challenge."
+    }
   })
+
   const scraper = new Scraper(req.query.repository)
   let result = await scraper.scrape()
-  // scr.print()
+
   res.json({
     status: 'Scraping ' + req.query.repository,
     date: new Date(),
--- src/Utils/Scraper.js
+++ src/Utils/Scraper.js
@@ -5,37 +5,22 @@ export default class Scraper {
   constructor(repository) {
     this.repository = repository;
     this.extensions = {};
-    this.scrapeList = [];
-    this.teste = 0;
-  }
-
-  async testee() {
-    this.teste += 1;
-    return this.teste
   }
 
   async getHtml(repository) {
-    return await axios.get(`https://github.com/${repository}`).then(r => r.data.toString()).catch(console.log)
+    return await axios.get(`https://github.com/${repository}`)
+      .then(r => r.data.toString())
+      .catch(({response}) => { return { error: response.status || response.statusCode || 'response?', message: 'Github says: Too many requests.' } })
   }
 
   async getHREFS(html) {
-    // let html = await this.getHtml()
     const regex = new RegExp('(?<=<a class="js-navigation-open link-gray-dark" title=".*" id=".*" href="/)(.*?)(?=">)', 'g')
     this.hrefs = html.match(regex)
     return this.hrefs
   }
 
-  async getTokens(hrefs) { // calling a token because i don't know yet if it is a folder or file
-    const regex = new RegExp('.*/', 'g')
-    // const hrefs = await this.getHREFS()
-    const tokens = hrefs.map(href => {
-      return href.replace(regex, "");
-    })
-    return tokens
-  }
-
   getSizeInBytes(str = "") {
-    const num = str.split(" ")[0]
+    let num = str.split(" ")[0]
     switch (str.split(" ")[1]) {
       case 'PB':
         num *= 1024
@@ -52,61 +37,15 @@ export default class Scraper {
     }
   }
 
-  async scrapeToken(repository) { // repository = href singular
+  async scrapeRepository(repository) { // repository = href singular
     const html = await this.getHtml(repository)
+    if (html.error === 429) return html // too many requests
     const hrefs = await this.getHREFS(html)
-    // return {hrefs, repository}
-    if (hrefs) {
-      return hrefs
-    } else {
-      const regex = new RegExp('.*/', 'g')
-      let fileName = repository.replace(regex, '').split('.')
-      const fileExtension = fileName[fileName.length - 1]
-      return { ...this.getFileInfo(html), ext: fileExtension }
-    }
-  }
-
-  async scrapeRecursive(hrefs) {
-    const regex = new RegExp('.*/', 'g')
-
-    for (const href of hrefs) {
-      const fileInfo = await this.scrapeToken(hrefs)
-      if (Array.isArray(fileInfo)) {
-        this.scrapeRecursive(href)
-      } else {
-        this.scrapeList.push({ fileName: fileInfo })
-      }
-    }
-  }
-
-  async buildScrapeList(repository) { // repository = href singular
-    const html = await this.getHtml(repository)
-    const hrefs = await this.getHREFS(html)
-    // return {hrefs, repository}
     if (hrefs) {
       for (const href of hrefs) {
-        this.scrapeList.push(await this.buildScrapeList(href))
+        await this.scrapeRepository(href)
       }
     } else {
-      console.log(repository)
-      const regex = new RegExp('.*/', 'g')
-      let fileName = repository.replace(regex, '').split('.')
-      const fileExtension = fileName[fileName.length - 1]
-      this.scrapeList.push({ [fileExtension]: { ...this.getFileInfo(html) } })  // {ext: 'py', size: 15441, lines: 154}
-    }
-    return [...this.scrapeList]
-  }
-
-  async buildScrapeExtensions(repository) { // repository = href singular
-    const html = await this.getHtml(repository)
-    const hrefs = await this.getHREFS(html)
-    // return {hrefs, repository}
-    if (hrefs) {
-      for (const href of hrefs) {
-        await this.buildScrapeExtensions(href)
-      }
-    } else {
-      // console.log(repository)
       const regex = new RegExp('.*/', 'g')
       let fileName = repository.replace(regex, '').split('.')
       const fileExtension = fileName[fileName.length - 1]
@@ -117,7 +56,6 @@ export default class Scraper {
         this.extensions[fileExtension].size += temp.size
         this.extensions[fileExtension].lines += temp.lines
       }
-      // this.scrapeList.push({ [fileExtension]: { ...this.getFileInfo(html) } })  // {ext: 'py', size: 15441, lines: 154}
     }
     return { ...this.extensions }
   }
@@ -125,9 +63,9 @@ export default class Scraper {
   async scrape() {
     console.log("Scraping " + this.repository);
     const timer = Date.now()
-    let result = await this.buildScrapeExtensions(this.repository)
+    let result = await this.scrapeRepository(this.repository)
     console.log(`done in ${Date.now() - timer}ms with ${this.repository}`);
-    return {seconds: (Date.now() - timer)/1000, ...result,}
+    return { seconds: (Date.now() - timer) / 1000, ...result, }
   }
 
   getFileInfo(html) {
@@ -138,6 +76,6 @@ export default class Scraper {
     const sizeMatch = div.match(/(?<=.*>\s*)(\d|\.)*( .?B)/gs)
     const size = sizeMatch ? this.getSizeInBytes(sizeMatch[0]) : 0;
     return { size, lines }
-  }
+  } // this function is in the end of the class simply because vscode glitches the colors after the regex and it annoys me
 
 }
