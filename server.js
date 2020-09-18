c96a131f936f53614a7c8fe587dc22ddb020e219
Matheus DF <51722889+matheusdf32@users.noreply.github.com>
2020-09-18 14:08:25 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import Scraper from './src/Utils/Scraper.js'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 const app = express()
 
 app.get('/', async (req, res) => {
